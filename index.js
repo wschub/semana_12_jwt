888ed07110e2223f86e887e969b94e94675df52c
Wolfgang Schubert <david.rodriguez@agileinnova.org>
2020-11-18 00:03:22 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+app.get('/api', (req, res) => {
+	res.json({
+		text: 'api jwt'
+	});
+});
+
+app.post('/api/login', (req, res) => {
+	
+	const { username,email,password } = req.body;
+
+	const user = {
+		username,
+		email,
+		password
+	};
+
+	const token = jwt.sign({ user }, 'my_secret_key');
+	res.json({token});
+});
+
+
+
+
+app.get('/api/privada', verificarToken ,(req, res) => {
+	jwt.verify(req.token, 'my_secret_key', (err, data) => {
+		if(err) {
+			res.sendStatus(403);
+		} else {
+			res.json({
+				text: 'Bienvenido página privada!',
+				data
+			});
+		}
+	});
+});
+
+function verificarToken(req, res, next) {
+	const bearerheader = req.headers["authorization"];
+	console.log(bearerheader)
+	if(typeof bearerheader !== 'undefined') {
+		const bearer = bearerheader.split(" ");
+		const bearerToken = bearer[1];
+		req.token = bearerToken;
+		next();
+	} else {
+		res.sendStatus(403);
+	}
+}
+
+
+app.listen(3000, () => {
+	console.log('jwt app corriendo en el puerto 3000!')
+});
